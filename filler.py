058c33c29601458eebb35ab474497c84800d8140
Ivan Modin <raijusiju@gmail.com>
2020-03-30 20:05:26 +0400

    fill reports via json instead of chromedriver

--- filler.py
+++ filler.py
@@ -1,25 +1,15 @@
 """
 This script runs second.
-Works via headless Chrome, so you should add path of chromedriver executable to the PATH.
 
-It will open every report page and scrap necessary information.
-It takes several hours to complete as there are almost 7000 reports already.
-Works via splitting page text by regexp and then accessing tokens by indexes in array, so it's pretty unstable.
+It will get every report in json and take necessary information.
+It takes a lot of time to fetch because there are so much reports.
 
 To use it without modifications you should put non-empty data.csv file
 in the same directory with this script (current data.csv is good).
 """
 
 import csv
-import re
-from selenium.webdriver import Chrome, ChromeOptions
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
-page_loading_timeout = 10
-regex = re.compile('<.*?>')
-split_regex = re.compile(r'\s{2,}')
+import requests
 
 
 def fill():
@@ -28,36 +18,21 @@ def fill():
         reader = csv.DictReader(file)
         for row in reader:
             reports.append(dict(row))
-    options = ChromeOptions()
-    options.add_argument('headless')
-    driver = Chrome(options=options)
     count_of_reports = len(reports)
     for i in range(count_of_reports):
         print('Fetching report ' + str(i + 1) + ' out of ' + str(count_of_reports))
-        report_url = 'https://' + reports[i]['link']
-        driver.get(report_url)
+        report_url = 'https://' + reports[i]['link'] + '.json'
         try:
-            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'routerlink')))
+            json_info = requests.get(report_url).json()
+            reports[i]['title'] = json_info['title']
+            reports[i]['program'] = json_info['team']['profile']['name']
+            reports[i]['upvotes'] = int(json_info['vote_count'])
+            reports[i]['bounty'] = float(json_info['bounty_amount']) if json_info['has_bounty?'] else 0.0
+            reports[i]['vuln_type'] = json_info['weakness']['name'] if 'weakness' in json_info else ''
         except Exception:
             print('error at report ' + str(i + 1))
             continue
-        raw_info = driver.find_elements_by_class_name('report-heading')[0]
-        html = raw_info.get_attribute('innerHTML')
-        text = re.sub(regex, ' ', html)
-        tokens = re.split(split_regex, text)
-        print(tokens)
-        try:
-            reported_to_index = tokens.index('Reported To')
-            reports[i]['program'] = tokens[reported_to_index + 1]
-        except Exception:
-            pass
-        reports[i]['title'] = tokens[5].replace('<', '\<').replace('>', '\>')
-        reports[i]['upvotes'] = int(tokens[3])
-        try:
-            bounty_index = tokens.index('Bounty')
-            reports[i]['bounty'] = float(tokens[bounty_index + 1][1:].replace(',', ''))
-        except Exception:
-            pass
+
         print(reports[i])
 
     with open('data.csv', 'w', newline='', encoding='utf-8') as file:
